0b8bfa40041cdfaa66cc9b174964b98cec620801
Vikram-repo <vikramazure02@gmail.com>
2026-01-08 18:34:34 +0530

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.54.0"
+    }
+  }
+}
+provider "azurerm" {
+  subscription_id = "313f3ea2-1c94-4db5-970f-0f7b58f52bce"
+  features {}
+}
+
+
+
+resource "azurerm_resource_group" "rg-b10" {
+  name     = "rg21"
+  location = "eastus"
+}
